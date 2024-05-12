3209fd2106f0376e1e3a2ab06784a9203520ea8e
atharvv01 <athzanwa@gmail.com>
2024-05-12 21:00:42 +0530

    created all required models

---
+++ backend/models/recipients_model.js
@@ -0,0 +1,29 @@
+// not required as of now 
+
+
+// const mongoose = require("mongoose");
+
+// const recipientSchema = new mongoose.Schema({
+//   sender: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "User",
+//     required: true,
+//   },
+//   receiver: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "User",
+//     required: true,
+//   },
+//   sharedFiles: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Upload",
+//     required: true
+//   },
+//   sharedDate: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Upload"
+//   },
+// });
+
+// const Recipient = mongoose.model("Recipient", recipientSchema);
+// module.exports = Recipient;
